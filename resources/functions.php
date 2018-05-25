7640a315e7d788459e60bc6331b2597925d0a661
Slyxxy <33089544+Slyxxy@users.noreply.github.com>
2018-05-25 20:56:18 +0200

    Dynamic changes on the hompage and categories. it calls from db. 019 is next on vid

--- resources/functions.php
+++ resources/functions.php
@@ -34,23 +34,51 @@ function fetch_array($result){
 
     }
 
-//Thumbnail display get products
-    function get_productS() {
+    //*************************** Front End Functions********************************* */
+    //Thumbnail display get products
+    function get_products() {
         $query = query("SELECT * FROM products");
         confirm($query);
+    
 
         while ($row = fetch_array($query)) {
-            
-        
-
-
-
-
+            $product = <<<EOD
+            <div class="col-sm-4 col-lg-4 col-md-4">
+            <div class="thumbnail">
+                <a href = "item.php?id={$row['product_id']}"><img src="{$row['product_image']}" alt=""></a>
+                <div class="caption">
+                    <h4 class="pull-right">&#8364;{$row['product_price']}</h4>
+                    <h4><a href="item.php?id={$row['product_id']}">{$row['product_title']}</a>
+                    </h4>
+                    <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
+                    <a class="btn btn-primary" target="_blank" href="item.php?id={$row['product_id']}">Add to cart</a>
+                </div>
+            </div>
+        </div>
+EOD;
+            echo $product; 
+           
+         
         }
+    }
+    function get_categories(){
+        $query = query("SELECT * FROM categories");
+        confirm($query);
+    
+        while ($row =  mysqli_fetch_array($query)) {
+       
+        $categories_links = <<<EOD
+        <a href='category.php?id={$row['cat_id']}' class='list-group-item'>{$row['cat_title']}</a>
+EOD;
 
-        
+echo $categories_links;
+        }
+    
+    //*************************** Back End Functions********************************* */
+
+
+
 
     }
-        
 
 ?>

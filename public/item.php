d4115e155cc2dcc11b34fe3447ec53dae4f5fafe
Slyxxy <33089544+Slyxxy@users.noreply.github.com>
2018-05-29 06:54:35 +0200

    dynamic display of categories, almost done

--- public/item.php
+++ public/item.php
@@ -5,8 +5,19 @@
 <div class="container">
 
        <!-- Side Navigation -->
-
+ 
            <?php include(TEMPLATE_FRONT . DS . "side_nav.php") ?>
+<?php    
+        $query = query("SELECT * FROM products WHERE product_id = " . escape_string($_GET['id'])."  ");
+        confirm($query);
+    
+
+        while ($row = fetch_array($query)):
+
+
+               
+ ?>
+
 <div class="col-md-9">
 
 <!--Row For Image and Short Description-->
@@ -14,7 +25,7 @@
 <div class="row">
 
     <div class="col-md-7">
-       <img class="img-responsive" src="http://placehold.it/700x600" alt="">
+       <img class="img-responsive" src="<?php echo $row['product_image']; ?>" alt="">
 
     </div>
 
@@ -24,9 +35,9 @@
          
 
     <div class="caption-full">
-        <h4><a href="#">Javascript Course</a> </h4>
+        <h4><a href="#"><?php echo $row['product_title']; ?></a> </h4>
         <hr>
-        <h4 class="">$24.99</h4>
+        <h4 class=""> <?php echo  "&#8364;" .$row['product_price']; ?></h4>
 
     <div class="ratings">
      
@@ -40,7 +51,7 @@
         </p>
     </div>
           
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+        <p><?php echo $row['short_desc']; ?></p>
 
    
     <form action="">
@@ -81,12 +92,7 @@
 
 <p></p>
            
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-
-
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+<p><?php echo $row['product_description']; ?><p>
 
     </div>
     <div role="tabpanel" class="tab-pane" id="profile">
@@ -191,7 +197,11 @@
 
 
 
-</div>
+
+
+</div><!-- col-md-9 ends here -->
+
+<?php endwhile; ?>
 
 </div>
     <!-- /.container -->
--- resources/functions.php
+++ resources/functions.php
@@ -74,11 +74,58 @@ EOD;
 echo $categories_links;
         }
     
+
+
+
+
+
+/*
+function get_products_in_cat_page(){
+
+$query = query("SELECT * FROM products WHERE product_category_id =" . escape_string($_GET['id']) . " ");
+confirm($query);
+
+
+
+while ($row = fetch_array($query)) {
+    $product = <<<EOD
+    <div class="col-md-3 col-sm-6 hero-feature">
+    <div class="thumbnail">
+        <img src="http://placehold.it/800x500" alt="">
+        <div class="caption">
+            <h3>Feature Label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            <p>
+                <a href="#" class="btn btn-primary">Buy Now!</a> <a href="#" class="btn btn-default">More Info</a>
+            </p>
+        </div>
+    </div>
+</div>}}
+
+EOD;
+
+ echo $product; 
+               
+             
+            }
+        }
+*/
+        
     //*************************** Back End Functions********************************* */
 
 
 
 
     }
+
+
+
+
+
+
+
+
+
+
 
 ?>
